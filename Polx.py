72af1689460ba9c1bdfd8dae04ba930a1883f816
Goof <107615523+Goofisded@users.noreply.github.com>
2024-02-18 00:08:08 +0530

    Create Polx.py

---
+++ Polx.py
@@ -0,0 +1,49 @@
+import os
+import requests
+
+def print_banner():
+    banner = """
+    [38;2;255;165;0m  
+██████╗  ██████╗ ██╗     ██╗  ██╗
+██╔══██╗██╔═══██╗██║     ╚██╗██╔╝
+██████╔╝██║   ██║██║      ╚███╔╝ 
+██╔═══╝ ██║   ██║██║      ██╔██╗ 
+██║     ╚██████╔╝███████╗██╔╝ ██╗
+╚═╝      ╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                 
+       
+    """
+    made_by = "[38;2;255;165;0m                      Made by Goof"
+    print(f"{banner.center(120)}\n{made_by.center(120)}\n")
+    input("[38;2;255;165;0mPress Enter to start...")
+    os.system('cls' if os.name == 'nt' else 'clear')  
+
+def check_subdomains(url, subdomains_file):
+    try:
+        with open(subdomains_file, 'r') as file:
+            subdomains = file.read().splitlines()
+
+        found_links = []
+
+        for subdomain in subdomains:
+            full_url = f"http://{subdomain}.{url}"
+            try:
+                response = requests.get(full_url)
+                if response.status_code == 200:
+                    found_links.append(full_url)
+                    print(full_url, "[Found]")
+            except requests.ConnectionError:
+                pass 
+
+        with open(f"{url}_Subdomains.txt", 'w') as output_file:
+            for found_link in found_links:
+                output_file.write(f"{found_link}\n")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    print_banner()
+    url = input("Enter the base URL (usage example.com): ")
+    subdomains_file = "subdomains.txt"
+    check_subdomains(url, subdomains_file)
